5ddfcae1a8464b62a9574e9972a98a2921c74d09
Smelky <46086711+Smelky@users.noreply.github.com>
2019-03-27 12:36:54 +0200

    Changed

--- src/test/java/TestValidateMachine.java
+++ src/test/java/TestValidateMachine.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 
 public class TestValidateMachine {
     private ValidateMachine validator;
@@ -12,22 +13,42 @@ public class TestValidateMachine {
     }
 
     @Test
-    public void testPhoneNumberValidator() {
-        assertFalse(validator.phoneNumberValidator("+380631021340"));
+    public void shouldTestValidPhoneNumber() {
+        assertTrue(validator.phoneNumberValidator("+380631021340"));
     }
 
     @Test
-    public void testEmailAdressValidator() {
-        assertFalse(validator.emailAddressValidator("12321@gmail.com"));
+    public void shouldTestInvalidPhoneNumber() {
+        assertFalse(validator.phoneNumberValidator("+3806"));
     }
 
     @Test
-    public void testDateValidator() {
-        assertFalse(validator.dateValidator("01.04.1998"));
+    public void shouldTestValidEmailAddress() {
+        assertTrue(validator.emailAddressValidator("12321@gmail.com"));
     }
 
     @Test
-    public void testIpAddressValidator() {
-        assertFalse(validator.ipAddressValidator("192.168.0.1"));
+    public void shouldTestInvalidEmailAddress() {
+        assertFalse(validator.emailAddressValidator("12321@com"));
+    }
+
+    @Test
+    public void shouldTestValidDate() {
+        assertTrue(validator.dateValidator("01.04.1998"));
+    }
+
+    @Test
+    public void shouldTestInvalidDate() {
+        assertFalse(validator.dateValidator("0104.198"));
+    }
+
+    @Test
+    public void shouldTestValidIpAddress() {
+        assertTrue(validator.ipAddressValidator("192.168.0.1"));
+    }
+
+    @Test
+    public void shouldTestInvalidIpAddress() {
+        assertFalse(validator.ipAddressValidator("192.168.1"));
     }
 }
